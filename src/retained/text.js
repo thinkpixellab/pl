0c303454fcb68c2141754e866e2ce6e7ea5753e7
Kevin Moore <kevin@thinkpixellab.com>
2011-08-21 14:45:37 -0700

    adding new properties to text…with warning

--- src/retained/text.js
+++ src/retained/text.js
@@ -1,7 +1,9 @@
+// TODO: 2011-09-21: note that changing properties besides width/height between frames will not invalidate cache
+
 goog.provide('pl.retained.Text');
 
+goog.require('pl.gfx');
 goog.require('pl.retained.Element');
-goog.require('goog.array');
 
 /**
  * @constructor
@@ -18,9 +20,15 @@ pl.retained.Text = function(value, width, height, opt_x, opt_y) {
 };
 goog.inherits(pl.retained.Text, pl.retained.Element);
 
+pl.retained.Text.prototype.textFillStyle = 'white';
+
+pl.retained.Text.prototype.font = '25px Helvetica';
+
+pl.retained.Text.prototype.lineHeight = 25;
+
 pl.retained.Text.prototype.draw = function(ctx) {
   ctx.font = '25px Helvetica';
   ctx.textBaseline = 'top';
-  ctx.fillStyle = 'white';
-  ctx.fillText(this._value, 0, 0, this.width);
+  ctx.fillStyle = this.textFillStyle;
+  pl.gfx.multiFillText(ctx, this._value, 0, 0, this.lineHeight, this.width);
 };
