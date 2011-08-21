c74fdf1c2be2edeefcf0d347bd37e222ec325a47
Kevin Moore <kevin@thinkpixellab.com>
2011-08-21 11:54:40 -0700

    adding ex and gfx

---
+++ ex.js
@@ -0,0 +1,64 @@
+goog.provide('pl.ex');
+
+goog.require('goog.style');
+goog.require('goog.string');
+goog.require('goog.array');
+goog.require('goog.math.Size');
+
+/**
+ * Sets a style value on an element.
+ *
+ * This function is not indended to patch issues in the browser's style
+ * handling, but to allow easy programmatic access to setting dash-separated
+ * style properties.  An example is setting a batch of properties from a data
+ * object without overwriting old styles.  When possible, use native APIs:
+ * elem.style.propertyKey = 'value' or (if obliterating old styles is fine)
+ * elem.style.cssText = 'property1: value1; property2: value2'.
+ *
+ * @param {Element} element The element to change.
+ * @param {string} style a style name
+ * @param {string|number|boolean} value
+ */
+pl.ex.setStyle = function(element, style, value){
+  goog.style.setStyle(element, style, value);
+  goog.array.forEach(pl.ex.prefixes, function(pf) {
+    var name = goog.string.buildString('-', pf, '-', style);
+    goog.style.setStyle(element, name, value);
+  });  
+};
+
+/**
+ * @param {HTMLCanvasElement} canvasElement
+ * @returns {goog.math.Size}
+ */
+pl.ex.getCanvasSize = function(canvasElement){
+  return new goog.math.Size(canvasElement.width, canvasElement.height);
+};
+
+pl.ex.requestAnimationFrame = function(callback){
+  var func = goog.global['requestAnimationFrame']       ||
+             goog.global['webkitRequestAnimationFrame'] ||
+             goog.global['mozRequestAnimationFrame']    ||
+             goog.global['oRequestAnimationFrame']      ||
+             goog.global['msRequestAnimationFrame']     ||
+             function(/* function */ callback, /* DOMElement */ element){
+               goog.global.setTimeout(callback, 1000 / 60);
+             };
+  func(callback);
+};
+
+/**
+ * Almost 10x faster than Math.round, per
+ * http://www.html5rocks.com/en/tutorials/canvas/performance/
+ * @param {number} number
+ * @returns {number}
+ */
+pl.ex.round = function(number){
+  return (0.5 + number) << 0;
+};
+
+/**
+ * @const
+ * @type Array.<string>
+ **/
+pl.ex.prefixes = "webkit moz o ms khtml".split(' ');
---
+++ gfx.js
@@ -0,0 +1,92 @@
+goog.provide('pl.gfx');
+
+goog.require('goog.math.Size');
+
+/**
+ * @param {string|CanvasGradient|CanvasPattern} fill
+ */
+pl.gfx.fillCircle = function(ctx, x, y, radius, fill) {
+  ctx.save();
+  ctx.fillStyle = fill;
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  ctx.restore();
+};
+
+// 2011-08-17
+// Discovered here: http://stackoverflow.com/questions/4478742/html5-canvas-can-i-somehow-use-linefeeds-in-filltext
+// Inspired by: http://stackoverflow.com/users/128165/gaby-aka-g-petrioli
+// Created by: http://stackoverflow.com/users/791890/jeffchan
+// Copied from: http://jsfiddle.net/jeffchan/WHgaY/76/
+/**
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {string} text
+ * @param {number} x
+ * @param {number} y
+ * @param {number} lineHeight
+ * @param {number} fitWidth
+ * @param {boolean=} opt_measureOnly
+ */
+pl.gfx.multiFillText = function(ctx, text, x, y, lineHeight, fitWidth, opt_measureOnly) {
+  opt_measureOnly = !!opt_measureOnly;
+
+  text = text.replace(/(\r\n|\n\r|\r|\n)/g, "\n");
+  var sections = text.split("\n");
+
+  var i, index, str, wordWidth, words, currentLine = 0,
+    maxHeight = 0,
+    maxWidth = 0;
+
+  var printNextLine = function(str) {
+    if (!opt_measureOnly) {
+      ctx.fillText(str, x, y + (lineHeight * currentLine));
+    }
+
+    currentLine++;
+    wordWidth = ctx.measureText(str).width;
+    if (wordWidth > maxWidth) {
+      maxWidth = wordWidth;
+    }
+  };
+
+  for (i = 0; i < sections.length; i++) {
+    words = sections[i].split(' ');
+    index = 1;
+
+    while (words.length > 0 && index <= words.length) {
+
+      str = words.slice(0, index).join(' ');
+      wordWidth = ctx.measureText(str).width;
+
+      if (wordWidth > fitWidth) {
+        if (index === 1) {
+          // Falls to this case if the first word in words[] is bigger than fitWidth
+          // so we print this word on its own line; index = 2 because slice is
+          str = words.slice(0, 1).join(' ');
+          words = words.splice(1);
+        } else {
+          str = words.slice(0, index - 1).join(' ');
+          words = words.splice(index - 1);
+        }
+
+        printNextLine(str);
+
+        index = 1;
+      } else {
+        index++;
+      }
+    }
+
+    // The left over words on the last line
+    if (index > 0) {
+      printNextLine(words.join(' '));
+    }
+
+  }
+
+  maxHeight = lineHeight * (currentLine);
+
+  return new goog.math.Size(maxWidth, maxHeight);
+};
