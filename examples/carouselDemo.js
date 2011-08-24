039b8e7075c8f3e3008eacaedcfb01af3c4f1195
Kevin Moore <kevin@thinkpixellab.com>
2011-08-24 12:57:35 -0700

    helper: bounds-based hit testing
    …with demo in carouselDemo

--- examples/carouselDemo.js
+++ examples/carouselDemo.js
@@ -2,18 +2,23 @@ goog.require('goog.debug.LogManager');
 goog.require('goog.math.Coordinate');
 goog.require('goog.Timer');
 goog.require('goog.math.Vec2');
+goog.require('goog.events');
+goog.require('goog.events.EventType');
 
 goog.require('pl.ex');
+goog.require('pl.gfx');
 goog.require('pl.retained.Stage');
 goog.require('pl.retained.Text');
 goog.require('pl.retained.CarouselContainer');
 goog.require('pl.retained.Animation');
+goog.require('pl.retained.Helper');
 goog.require('pl.FpsLogger');
 goog.require('pl.DebugDiv');
 
 var fpsLogger, logger;
-var stage, container;
+var stage;
 var animation;
+var mouse;
 
 
 
@@ -27,7 +32,10 @@ function init() {
   /** @type {!HTMLCanvasElement} */
   (document.getElementById('content'));
 
-  container = new pl.retained.CarouselContainer(500, 500);
+  goog.events.listen(canvas, goog.events.EventType.MOUSEOUT, onMouseOut);
+  goog.events.listen(canvas, goog.events.EventType.MOUSEMOVE, onMouseMove);
+
+  var container = new pl.retained.CarouselContainer(500, 500);
   container.radius(new goog.math.Size(250, 20));
 
   for (var i = 0; i < 10; i++) {
@@ -51,15 +59,32 @@ function init() {
 
 
 
+function onMouseMove(e) {
+  mouse = new goog.math.Coordinate(e.offsetX, e.offsetY);
+}
+
+
+
+function onMouseOut(e) {
+  mouse = null;
+}
+
+
+
 function update() {
+  //goog.Timer.callOnce(update, 100);
+  pl.ex.requestAnimationFrame(update);
+
   fpsLogger.AddInterval();
 
   animation.tick();
 
   stage.draw();
 
-  //goog.Timer.callOnce(update);
-  pl.ex.requestAnimationFrame(update);
+  if (mouse) {
+    var ctx = stage.getContext();
+    pl.retained.Helper.borderHitTest(stage, mouse.x, mouse.y);
+  }
 }
 
 
--- src/retained/helper.js
+++ src/retained/helper.js
@@ -17,12 +17,20 @@ pl.retained.Helper.getTransformedCorners = function(element) {
   // x0,y0,x1,y1....xn,yn
   var flat = [0, 0, element.width, 0, element.width, element.height, 0, element.height];
 
+  pl.retained.Helper.getTransform(element).transform(flat, 0, flat, 0, 4);
+  return pl.ex.expandPoints(flat);
+};
+
+/**
+ * @param {!pl.retained.Element} element
+ * @returns {!goog.graphics.AffineTransform}
+ */
+pl.retained.Helper.getTransform = function(element) {
   var tx = goog.graphics.AffineTransform.getTranslateInstance(element.x, element.y);
   if (element.transform) {
     tx.concatenate(element.transform);
   }
-  tx.transform(flat, 0, flat, 0, 4);
-  return pl.ex.expandPoints(flat);
+  return tx;
 };
 
 /**
@@ -40,16 +48,81 @@ pl.retained.Helper.borderElements = function(stage) {
 /**
  * @param {!CanvasRenderingContext2D} ctx
  * @param {!pl.retained.Element} element
+ * @param {boolean=} opt_excludeChildren
  */
-pl.retained.Helper._borderElement = function(ctx, element) {
+pl.retained.Helper._borderElement = function(ctx, element, opt_excludeChildren) {
   var corners = pl.retained.Helper.getTransformedCorners(element);
 
   pl.gfx.lineToPath(ctx, corners);
 
-  ctx.save();
-  ctx.translate(element.x, element.y);
-  goog.array.forEach(element.getVisualChildren(), function(e) {
-    pl.retained.Helper._borderElement(ctx, e);
-  });
-  ctx.restore();
+  if (!opt_excludeChildren) {
+    ctx.save();
+    ctx.translate(element.x, element.y);
+    goog.array.forEach(element.getVisualChildren(), function(e) {
+      pl.retained.Helper._borderElement(ctx, e);
+    });
+    ctx.restore();
+  }
+};
+
+/**
+ * @param {!pl.retained.Stage} stage
+ * @param {number} x
+ * @param {number} y
+ */
+pl.retained.Helper.hitTest = function(stage, x, y) {
+  return pl.retained.Helper._hitTest(stage.getRoot(), x, y);
+};
+
+/**
+ * @param {!pl.retained.Element} element
+ * @param {number} x
+ * @param {number} y
+ */
+pl.retained.Helper._hitTest = function(element, x, y) {
+  var point = [x, y];
+  pl.retained.Helper.getTransform(element).createInverse().transform(point, 0, point, 0, 1);
+
+  var c = new goog.math.Coordinate(point[0], point[1]);
+  var bounds = new goog.math.Rect(0, 0, element.width, element.height);
+
+  var hits = [];
+  if (bounds.contains(c)) {
+
+    var children = element.getVisualChildren();
+
+    for (var i = 0; i < children.length; i++) {
+      var e = children[i];
+      hits = pl.retained.Helper._hitTest(e, c.x, c.y);
+      if (hits.length) {
+        break;
+      }
+    }
+    goog.array.insertAt(hits, element);
+    return hits;
+  } else {
+    return [];
+  }
+};
+
+/**
+ * @param {!pl.retained.Stage} stage
+ */
+pl.retained.Helper.borderHitTest = function(stage, x, y) {
+  var ctx = stage.getContext();
+
+  pl.gfx.fillCircle(ctx, x, y, 10, 'blue');
+
+  var hits = pl.retained.Helper.hitTest(stage, x, y);
+  if (hits.length) {
+
+    ctx.strokeStyle = 'green';
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    goog.array.forEach(hits, function(e) {
+      pl.retained.Helper._borderElement(ctx, e, true);
+    });
+    ctx.stroke();
+  }
 };
