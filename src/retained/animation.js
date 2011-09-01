a2aab6c675012926fe94442afd775214d16de134
Kevin Moore <kevin@thinkpixellab.com>
2011-08-31 20:52:20 -0700

    animation: added an optional 'end' function

--- src/retained/animation.js
+++ src/retained/animation.js
@@ -10,14 +10,16 @@ goog.require('pl.retained.EventType');
  * @param {!pl.retained.Element} element
  * @param {number} frameCount
  * @param {!function(number, !pl.retained.Element)} func
+ * @param {function(!pl.retained.Element)=} opt_finishFunc
  */
-pl.retained.Animation = function(element, frameCount, func) {
+pl.retained.Animation = function(element, frameCount, func, opt_finishFunc) {
   goog.base(this, element);
 
   element.addEventListener(pl.retained.EventType.UPDATE, this._tick, false, this);
 
   this._frameCount = frameCount;
   this._func = func;
+  this._finishFunc = opt_finishFunc || goog.nullFunction;
   this._frame = 0;
 };
 goog.inherits(pl.retained.Animation, pl.Behavior);
@@ -35,12 +37,16 @@ pl.retained.Animation.prototype._tick = function() {
   var element = this.getElement();
   goog.asserts.assert(element);
   this._func(this._frame, element);
-  this._frame = (this._frame + 1) % this._frameCount;
+  this._frame++;
+  if (this._frame == this._frameCount) {
+    this._finishFunc(element);
+  }
+  this._frame = this._frame % this._frameCount;
 };
 
 /**
-  @return {pl.retained.Element}
-  */
+ @return {pl.retained.Element}
+ */
 pl.retained.Animation.prototype.getElement = function() {
   return /** @type {pl.retained.Element} */ (this._element);
 };
