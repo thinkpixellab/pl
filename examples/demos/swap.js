53eb6d6fcb519bfb990c9af831d9bf09342e6aa4
Kevin Moore <kevin@thinkpixellab.com>
2011-08-31 20:53:21 -0700

    fun in swap

--- examples/demos/swap.js
+++ examples/demos/swap.js
@@ -12,33 +12,49 @@ goog.require('pl.retained.Container');
 goog.require('pl.retained.Stage');
 goog.require('pl.retained.Text');
 
-
 /**
  * @constructor
  */
 demos.Swap = function(canvas) {
-  var text = new pl.retained.Text('Swap!', 400, 400);
-  text.fillStyle = 'blue';
-  text.multiLine = true;
 
   var container = new pl.retained.Container(400, 400);
-  container.addElement(text);
 
   this._stage = new pl.retained.Stage(canvas, container);
 
-  var animation = new pl.retained.Animation(container, 100, function(i, element) {
-    if (i === 0) {
-      element.parentTransform = new goog.graphics.AffineTransform();
-    }
-    var transform = element.parentTransform;
-    var scale = 0.9;
-    transform.translate(200 / scale - 200, 200 / scale - 200);
-    transform.scale(scale, scale);
-  });
+
+  this._count = 0;
 };
 
 demos.Swap.description = 'Swap';
 
 demos.Swap.prototype.frame = function() {
   this._stage.draw();
+
+  if (this._count === 0) {
+    var text = demos.Swap.createText();
+    this._stage._element.insertAt(text);
+  }
+  this._count++;
+  this._count = this._count % 20;
+
+};
+
+demos.Swap.createText = function() {
+  var text = new pl.retained.Text('Swap!', 400, 400);
+  text.parentTransform = new goog.graphics.AffineTransform();
+  text.fillStyle = 'blue';
+  text.multiLine = true;
+
+  var frameCount = 100;
+  var animation = new pl.retained.Animation(text, frameCount, function(i, element) {
+    var ratio = (frameCount - i) / frameCount;
+    pl.gfx.affineOffsetScale(element.parentTransform, ratio, ratio, element.width / 2, element.height / 2);
+    element.alpha = ratio;
+    element.invalidateDraw();
+  },
+  function(element) {
+    element._parent.remove(element);
+  });
+
+  return text;
 };
