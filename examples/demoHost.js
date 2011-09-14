6d2b148e347daf17b1e3b10fbe92c10dcb62b640
Kevin Moore <kevin@thinkpixellab.com>
2011-09-14 14:34:53 -0700

    examples

---
+++ examples/demoHost.js
@@ -0,0 +1,129 @@
+goog.provide('DemoHost');
+
+goog.require('demos');
+goog.require('goog.History');
+goog.require('goog.Timer');
+goog.require('goog.asserts');
+goog.require('goog.debug.LogManager');
+goog.require('goog.dom');
+goog.require('goog.object');
+goog.require('goog.string');
+goog.require('goog.style');
+goog.require('goog.ui.Component.EventType');
+goog.require('goog.ui.MenuItem');
+goog.require('goog.ui.Select');
+goog.require('pl.DebugDiv');
+goog.require('pl.FpsLogger');
+goog.require('pl.ex');
+goog.require('pl.images');
+
+/**
+ * @constructor
+ */
+DemoHost = function() {
+  pl.DebugDiv.enable();
+  goog.style.setUnselectable(document.body, true);
+
+  this._logger = goog.debug.LogManager.getRoot();
+  this._fpsLogger = new pl.FpsLogger();
+
+  //
+  // Demo Selector
+  //
+  this._selectControl = new goog.ui.Select('Pick a demo...');
+  goog.object.forEach(demos.all, function(e, k, o) {
+    this._selectControl.addItem(new goog.ui.MenuItem(k));
+  },
+  this);
+  this._selectControl.render(goog.dom.getElement('DemoSelect'));
+
+  goog.events.listen(this._selectControl, goog.ui.Component.EventType.ACTION, function(e) {
+    var value = goog.string.urlEncode(e.target.getValue());
+    this._history.setToken(value);
+  },
+  false, this);
+
+  //
+  // History!
+  //
+  var historyElement =
+  /** @type {!HTMLInputElement} */
+  document.getElementById('history_state');
+
+  this._history = new goog.History(false, undefined, historyElement);
+  this._history.addEventListener(goog.history.EventType.NAVIGATE, this._navigate, false, this);
+  this._history.setEnabled(true);
+
+  this._frameFunc = goog.bind(this._drawFrame, this);
+
+  this._drawFrame();
+  this._updateHUD();
+};
+
+/**
+ * @export
+ */
+DemoHost.load = function() {
+  DemoHost.images = new pl.images({
+    'stars': 'resources/stars.png',
+    'pixellab': 'resources/pixellab.png'
+  });
+  DemoHost.images.load(function(p) {},
+  function() {
+    goog.global['$demoHost'] = new DemoHost();
+  });
+};
+
+DemoHost.images = null;
+
+DemoHost.prototype._frameMs = 0;
+
+DemoHost.prototype._navigate = function(e) {
+  var demoName;
+  if (e.token.length === 0) {
+    demoName = goog.object.getAnyKey(demos.all);
+  } else {
+    demoName = goog.string.urlDecode(e.token);
+  }
+  var demo = demos.all[demoName];
+  goog.asserts.assert(demo, 'should have a valid demo here!');
+  this._selectControl.setValue(demoName);
+  this._loadDemo(demo);
+};
+
+DemoHost.prototype._loadDemo = function(demoCtr) {
+  var newCanvas = goog.dom.createDom('canvas', {
+    'id': 'content',
+    'width': 500,
+    'height': 500
+  });
+  goog.dom.replaceNode(newCanvas, document.getElementById('content'));
+
+  this._demo = new demoCtr(newCanvas);
+};
+
+DemoHost.prototype._drawFrame = function() {
+  this._fpsLogger.AddInterval();
+
+  if (this._demo) {
+    this._demo.frame();
+  }
+
+  this._requestFrame();
+};
+
+DemoHost.prototype._requestFrame = function() {
+  if (this._frameMs) {
+    goog.Timer.callOnce(this._frameFunc, this._frameMs);
+  } else {
+    pl.ex.requestAnimationFrame(this._frameFunc);
+  }
+};
+
+DemoHost.prototype._updateHUD = function() {
+  pl.DebugDiv.clear();
+  this._logger.info(String(this._fpsLogger.fps));
+
+  var func = goog.bind(this._updateHUD, this);
+  goog.Timer.callOnce(func, 2000);
+};
---
+++ examples/demos/tile.js
@@ -0,0 +1,41 @@
+goog.provide('demos.Tile');
+
+goog.require('goog.math.Vec2');
+goog.require('pl.retained.Panel');
+goog.require('pl.retained.Stage');
+goog.require('pl.retained.TileLayer');
+
+
+/**
+ * @constructor
+ */
+demos.Tile = function(canvas) {
+  var image = DemoHost.images.get('stars');
+
+  var container = new pl.retained.Panel(500, 500);
+
+  this._tiles = [];
+
+  var size = new goog.math.Size(image.width, image.height);
+  for (var i = 0; i < 3; i++) {
+    var tileSize = size.clone().scale(Math.pow(2, -i));
+    var t = new pl.retained.TileLayer(500, 500, image, tileSize);
+    this._tiles.push(t);
+    container.addElement(t);
+  }
+
+  this._offset = new goog.math.Vec2(0, 0);
+
+  this._stage = new pl.retained.Stage(canvas, container);
+};
+
+demos.Tile.prototype.frame = function() {
+  this._offset.x -= 10;
+  for (var i = 0; i < this._tiles.length; i++) {
+    var t = this._tiles[i];
+    var o = this._offset.clone().scale(Math.pow(2, -i));
+    t.setOffset(o);
+  }
+
+  this._stage.draw();
+};
