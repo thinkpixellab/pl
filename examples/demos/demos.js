aed7c99d330c2d6456ce38c1741fd0222bffb541
Kevin Moore <kevin@thinkpixellab.com>
2011-09-28 09:44:39 -0700

    demos:squares

--- examples/demos/demos.js
+++ examples/demos/demos.js
@@ -5,6 +5,7 @@ goog.require('demos.Demo');
 goog.require('demos.NavLayerDemo');
 goog.require('demos.RotateTextDemo');
 goog.require('demos.ScaleDemo');
+goog.require('demos.Squares');
 goog.require('demos.SwapDemo');
 goog.require('demos.TileDemo');
 
@@ -12,6 +13,7 @@ goog.require('demos.TileDemo');
  * @type {Object.<string, function(new:demos.Demo, !HTMLCanvasElement)>}
  **/
 demos.all = {
+  'Squares': demos.Squares,
   'Carousel': demos.CarouselDemo,
   'Scale': demos.ScaleDemo,
   'Swap': demos.SwapDemo,
