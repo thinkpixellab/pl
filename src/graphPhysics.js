aad7435f4c852437eae76baaa96fe30d9da2ea5a
Kevin Moore <kevin@thinkpixellab.com>
2011-10-03 18:37:49 -0700

    graphPhysics: playing with goog.scope

--- src/graphPhysics.js
+++ src/graphPhysics.js
@@ -6,166 +6,171 @@ goog.require('goog.math.Vec2');
 goog.require('pl.Graph');
 goog.require('pl.GraphNode');
 
-/**
- * @param {!pl.Graph} graph
- * @param {!function(*,!pl.GraphNode)} setter
- * @param {!goog.math.Size} stageSize
- * @return {!goog.math.Vec2}
- */
-pl.graphPhysics.initializeGraph = function(graph, setter, stageSize) {
-  var box = null;
-  goog.iter.forEach(graph.getNodes(), function(node) {
-    var d = new pl.GraphNode(node, stageSize.width, stageSize.height);
-    setter(node, d);
-    box = pl.graphPhysics._boxIncludeCoordinate(d.position, box);
-  });
+goog.scope(function() {
+  var p = pl.graphPhysics;
 
-  return pl.graphPhysics._centerForceFromBox(box, stageSize);
-};
+  /**
+   * @param {!pl.Graph} graph
+   * @param {!function(*,!pl.GraphNode)} setter
+   * @param {!goog.math.Size} stageSize
+   * @return {!goog.math.Vec2}
+   */
+  p.initializeGraph = function(graph, setter, stageSize) {
+    var box = null;
+    goog.iter.forEach(graph.getNodes(), function(node) {
+      var d = new pl.GraphNode(node, stageSize.width, stageSize.height);
+      setter(node, d);
+      box = p._boxIncludeCoordinate(d.position, box);
+    });
 
-/**
- * @param {!pl.Graph} graph
- * @param {!goog.math.Vec2} centerForce
- * @param {number} version
- * @param {!function(*):!pl.GraphNode} mapper
- * @param {!goog.math.Size} stageSize
- * @return {boolean}
- */
-pl.graphPhysics.calculateGraph = function(graph, centerForce, version, mapper, stageSize) {
-  // go over every pair of nodes and calculate the pair forces
-  goog.iter.forEach(graph.getPairs(), function(pair) {
-    pl.graphPhysics._calculateForces(graph, mapper(pair[0]), mapper(pair[1]), version);
-  });
+    return p._centerForceFromBox(box, stageSize);
+  };
 
-  var updated = false;
+  /**
+   * @param {!pl.Graph} graph
+   * @param {!goog.math.Vec2} centerForce
+   * @param {number} version
+   * @param {!function(*):!pl.GraphNode} mapper
+   * @param {!goog.math.Size} stageSize
+   * @return {boolean}
+   */
+  p.calculateGraph = function(graph, centerForce, version, mapper, stageSize) {
+    // go over every pair of nodes and calculate the pair forces
+    goog.iter.forEach(graph.getPairs(), function(pair) {
+      p._calculateForces(graph, mapper(pair[0]), mapper(pair[1]), version);
+    });
 
-  var box = null;
-  // now go every node and do the velocity and location math
-  goog.iter.forEach(graph.getNodes(), function(node) {
-    var d = mapper(node);
-    d.force.add(centerForce);
-    updated = pl.graphPhysics._updateNode(d) || updated;
-    box = pl.graphPhysics._boxIncludeCoordinate(d.position, box);
-  });
+    var updated = false;
 
-  var newForce = pl.graphPhysics._centerForceFromBox(box, stageSize);
-  pl.ex.setVec(centerForce, newForce.x, newForce.y);
+    var box = null;
+    // now go every node and do the velocity and location math
+    goog.iter.forEach(graph.getNodes(), function(node) {
+      var d = mapper(node);
+      d.force.add(centerForce);
+      updated = p._updateNode(d) || updated;
+      box = p._boxIncludeCoordinate(d.position, box);
+    });
 
-  if (centerForce.magnitude() < pl.graphPhysics.SignificantMagnitude) {
-    pl.ex.clearVec(centerForce);
-  } else {
-    updated = true;
-  }
+    var newForce = p._centerForceFromBox(box, stageSize);
+    pl.ex.setVec(centerForce, newForce.x, newForce.y);
 
-  return updated;
-};
+    if (centerForce.magnitude() < p.SignificantMagnitude) {
+      pl.ex.clearVec(centerForce);
+    } else {
+      updated = true;
+    }
 
-/**
- * @private
- * @param {!pl.GraphNode} node
- * @return {boolean}
- */
-pl.graphPhysics._updateNode = function(node) {
-  // apply drag
-  node.velocity.scale(0.9);
+    return updated;
+  };
 
-  // apply force
-  node.velocity.add(node.force);
+  /**
+   * @private
+   * @param {!pl.GraphNode} node
+   * @return {boolean}
+   */
+  p._updateNode = function(node) {
+    // apply drag
+    node.velocity.scale(0.9);
 
-  var velocityMag = node.velocity.magnitude();
+    // apply force
+    node.velocity.add(node.force);
 
-  // terminal velocity
-  if (velocityMag > pl.graphPhysics.TerminalVelocity) {
-    node.velocity.scale(pl.graphPhysics.TerminalVelocity / velocityMag);
-    velocityMag = pl.graphPhysics.TerminalVelocity;
-  }
+    var velocityMag = node.velocity.magnitude();
 
-  if (velocityMag < pl.graphPhysics.SignificantMagnitude && node.force.magnitude() < pl.graphPhysics.SignificantMagnitude) {
-    pl.ex.clearVec(node.velocity);
-    pl.ex.clearVec(node.force);
-    return false;
-  } else {
-    node.position = goog.math.Coordinate.sum(node.position, node.velocity);
-    return true;
-  }
-};
+    // terminal velocity
+    if (velocityMag > p.TerminalVelocity) {
+      node.velocity.scale(p.TerminalVelocity / velocityMag);
+      velocityMag = p.TerminalVelocity;
+    }
 
-/**
- * @private
- * @param {!pl.Graph} graph
- * @param {!pl.GraphNode} d1
- * @param {!pl.GraphNode} d2
- * @param {number} version
- */
-pl.graphPhysics._calculateForces = function(graph, d1, d2, version) {
-  d1.ensureVersion(version);
-  d2.ensureVersion(version);
+    if (velocityMag < p.SignificantMagnitude && node.force.magnitude() < p.SignificantMagnitude) {
+      pl.ex.clearVec(node.velocity);
+      pl.ex.clearVec(node.force);
+      return false;
+    } else {
+      node.position = goog.math.Coordinate.sum(node.position, node.velocity);
+      return true;
+    }
+  };
 
-  // f will be the force between node1 and node2
-  // it will be the force applied to node1
-  // when applied to node2, it'll be flipped
-  var delta = goog.math.Vec2.difference(d2.position, d1.position);
-  var dmag = delta.magnitude();
+  /**
+   * @private
+   * @param {!pl.Graph} graph
+   * @param {!pl.GraphNode} d1
+   * @param {!pl.GraphNode} d2
+   * @param {number} version
+   */
+  p._calculateForces = function(graph, d1, d2, version) {
+    d1.ensureVersion(version);
+    d2.ensureVersion(version);
 
-  //
-  // Mild, constant attraction
-  //
-  var f = delta.clone().normalize().scale(0.005);
+    // f will be the force between node1 and node2
+    // it will be the force applied to node1
+    // when applied to node2, it'll be flipped
+    var delta = goog.math.Vec2.difference(d2.position, d1.position);
+    var dmag = delta.magnitude();
 
-  //
-  // global repulsion 1/(d^2)
-  //
-  f.add(delta.clone().invert().normalize().scale(100 / (dmag * dmag)));
+    //
+    // Mild, constant attraction
+    //
+    var f = delta.clone().normalize().scale(0.005);
 
-  // connected attraction
-  if (graph.containsEdge(d1.node, d2.node)) {
-    f.add(delta.clone().scale(dmag / 500));
-  }
+    //
+    // global repulsion 1/(d^2)
+    //
+    f.add(delta.clone().invert().normalize().scale(100 / (dmag * dmag)));
 
-  d1.force.add(f);
-  d2.force.subtract(f);
-};
+    // connected attraction
+    if (graph.containsEdge(d1.node, d2.node)) {
+      f.add(delta.clone().scale(dmag / 500));
+    }
 
-/**
- * @private
- * @param {goog.math.Box} box
- * @param {!goog.math.Size} otherSize
- * @return {!goog.math.Vec2}
- */
-pl.graphPhysics._centerForceFromBox = function(box, otherSize) {
-  if (box) {
-    var myCenter = new goog.math.Vec2(otherSize.width / 2, otherSize.height / 2);
-    var boxCenter = new goog.math.Vec2((box.left + box.right) / 2, (box.top + box.bottom) / 2);
-    return goog.math.Vec2.difference(myCenter, boxCenter).scale(0.0005);
-  } else {
-    return new goog.math.Vec2(0, 0);
-  }
-};
+    d1.force.add(f);
+    d2.force.subtract(f);
+  };
 
-/**
- * @private
- * @param {!goog.math.Coordinate} coordinate
- * @param {goog.math.Box} box
- * @return {!goog.math.Box}
- */
-pl.graphPhysics._boxIncludeCoordinate = function(coordinate, box) {
-  var b = new goog.math.Box(coordinate.y, coordinate.x, coordinate.y, coordinate.x);
-  if (!box) {
-    box = b;
-  } else {
-    box.expandToInclude(b);
-  }
-  return box;
-};
+  /**
+   * @private
+   * @param {goog.math.Box} box
+   * @param {!goog.math.Size} otherSize
+   * @return {!goog.math.Vec2}
+   */
+  p._centerForceFromBox = function(box, otherSize) {
+    if (box) {
+      var myCenter = new goog.math.Vec2(otherSize.width / 2, otherSize.height / 2);
+      var boxCenter = new goog.math.Vec2((box.left + box.right) / 2, (box.top + box.bottom) / 2);
+      return goog.math.Vec2.difference(myCenter, boxCenter).scale(0.0005);
+    } else {
+      return new goog.math.Vec2(0, 0);
+    }
+  };
 
-/**
- * @const
- * @type {number}
- */
-pl.graphPhysics.TerminalVelocity = 10;
+  /**
+   * @private
+   * @param {!goog.math.Coordinate} coordinate
+   * @param {goog.math.Box} box
+   * @return {!goog.math.Box}
+   */
+  p._boxIncludeCoordinate = function(coordinate, box) {
+    var b = new goog.math.Box(coordinate.y, coordinate.x, coordinate.y, coordinate.x);
+    if (!box) {
+      box = b;
+    } else {
+      box.expandToInclude(b);
+    }
+    return box;
+  };
 
-/**
- * @const
- * @type {number}
- */
-pl.graphPhysics.SignificantMagnitude = 0.01;
+  /**
+   * @const
+   * @type {number}
+   */
+  p.TerminalVelocity = 10;
+
+  /**
+   * @const
+   * @type {number}
+   */
+  p.SignificantMagnitude = 0.01;
+
+});
