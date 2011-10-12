281fe2bddd2fc898e2f2bb679d9903b888ebbb69
Kevin Moore <kevin@thinkpixellab.com>
2011-10-11 22:51:24 -0700

    deps

--- examples/demos/graphDemo.js
+++ examples/demos/graphDemo.js
@@ -17,7 +17,11 @@ goog.require('pl.retained.GraphElement');
 demos.GraphDemo = function(canvas) {
   var g = demos.GraphDemo._createGraph();
   var gp = new pl.GraphPhysics(g, pl.ex.getCanvasSize(canvas));
-  var graphElement = new pl.retained.GraphElement(gp, canvas.width, canvas.height);
+  gp.Inertia = 0.95;
+  gp.GlobalAttraction = 0;
+  gp.RepulsionFactor = 1500;
+  gp.ConnectionAttraction = 0.001 / 10;
+  var graphElement = new pl.retained.GraphElement(gp, canvas.width, canvas.height, undefined, demos.GraphDemo._factory);
 
   this._dragger = new goog.fx.Dragger(canvas);
   this._dragger.addEventListener(goog.fx.Dragger.EventType.START, this._onDragStart, undefined, this);
@@ -91,22 +95,21 @@ demos.GraphDemo.prototype._updateCursor = function() {
 };
 
 demos.GraphDemo._createGraph = function() {
-  var g = new pl.Graph();
+  var map = demos.GraphDemo._populateMap('pl.Graph');
 
-  for (var j = 0; j < 5; j++) {
-    var n = 5 + goog.math.randomInt(10);
-    var nodes = [];
-    for (var i = 0; i < n; i++) {
-      nodes.push(new demos.GraphDemo.Node(i));
-    }
-    for (var i = 0; i < nodes.length; i++) {
-      var adjacent = [new demos.GraphDemo.Node(i * i)];
-      if ((i + 1) < nodes.length) {
-        adjacent.push(nodes[i + 1]);
-      }
-      g.addNode(nodes[i], adjacent);
-    }
-  }
+  var realMap = {};
+  goog.object.forEach(map, function(element, key, map) {
+    realMap[key] = new demos.GraphDemo.Node(key);
+  });
+
+  var g = new pl.Graph();
+  goog.object.forEach(map, function(element, key, map) {
+    var adj = goog.array.map(element['requirements'], function(adjacentName) {
+      return realMap[adjacentName];
+    });
+    g.addNode(realMap[key], adj);
+  });
+
   return g;
 };
 
@@ -123,4 +126,60 @@ demos.GraphDemo.Node = function(value) {
  */
 demos.GraphDemo.Node.prototype.toString = function() {
   return String(this.value);
+};
+
+/**
+ * @private
+ * @param {*} node
+ * @return {!pl.retained.Element}
+ */
+demos.GraphDemo._factory = function(node) {
+  var size = new goog.math.Size(150, 25);
+  var canvas = new pl.retained.Canvas(size.width, size.height, true);
+
+  var shape = new pl.retained.Shape(size.width, size.height);
+  shape.fillStyle = '#333';
+  shape.type = pl.retained.ShapeType.ELLIPSE;
+  canvas.addElement(shape);
+
+  var text = new pl.retained.Text(String(node), size.width, 15);
+  text.isCentered = true;
+  text.font = '11px Helvetica, Arial, sans-serif';
+  canvas.addElement(text);
+  canvas.center(text, new goog.math.Coordinate(size.width / 2, size.height / 2));
+
+  canvas.addTransform().setToTranslation(size.width / -2, size.height / -2);
+  return canvas;
+};
+
+demos.GraphDemo._populateMap = function(name) {
+  var map = {};
+  demos.GraphDemo._populateMapCore(name, map);
+  return map;
+};
+
+demos.GraphDemo._populateMapCore = function(name, map) {
+  var node = goog.object.get(map, name);
+  if (!node) {
+    var deps = goog.dependencies_;
+    var path = deps.nameToPath[name];
+    var requires = goog.object.getKeys(deps.requires[path]);
+    var prunedRequires = goog.array.clone(requires);
+    var trans = [];
+    goog.array.forEach(requires, function(r) {
+      var rr = demos.GraphDemo._populateMapCore(r, map);
+      goog.array.forEach(rr, function(d) {
+        goog.array.remove(prunedRequires, d);
+      });
+      goog.array.extend(trans, rr);
+    });
+    goog.array.extend(trans, prunedRequires);
+    goog.array.removeDuplicates(trans);
+    node = map[name] = {
+      'name': name,
+      'requirements': prunedRequires,
+      'trans': trans
+    };
+  }
+  return node['trans'];
 };
