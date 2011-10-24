0862e8716ea3c98d109056316fda24326d3e9324
Kevin Moore <kevin@thinkpixellab.com>
2011-10-24 14:36:33 -0700

    starting on model

---
+++ src/data/model.js
@@ -0,0 +1,21 @@
+goog.provide('pl.data.Model');
+
+goog.require('goog.events.EventTarget');
+
+/**
+ * @constructor
+ * @extends {goog.events.EventTarget}
+ */
+pl.data.Model = function() {
+
+};
+goog.inherits(pl.data.Model, goog.events.EventTarget);
+
+goog.scope(function() {
+  var c = pl.data.Model;
+
+  goog.scope(function() {
+    var p = c.prototype;
+
+  });
+});
